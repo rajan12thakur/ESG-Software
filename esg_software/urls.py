c68868e9d6714bafbd8ba64b30268778a6df0726
pratikraut002 <devpratik.raut@gmail.com>
2026-07-11 17:11:12 +0530

    update

--- esg_software/urls.py
+++ esg_software/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from core.views import home_view
+
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path('platform/', include('platform_admin.urls')),
     path('auth/', include('authentication.urls')),
